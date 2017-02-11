4eb54c3fb5fa0bc573598163faaad4bd05d9c413
Daniel Pan <dprat0821@gmail.com>
2017-02-11 12:35:24 -0500

    init commit for basic layout

---
+++ Reasons/Reasons/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Reasons
+//
+//  Created by Daniel Pan on 11/02/2017.
+//  Copyright © 2017 geekmouse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Reasons/Reasons/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Reasons
+//
+//  Created by Daniel Pan on 11/02/2017.
+//  Copyright © 2017 geekmouse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
